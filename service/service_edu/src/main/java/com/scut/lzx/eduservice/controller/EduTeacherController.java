5e72f8ed697ec82c088cd4e440ef2cc241ef5ab9
rookie <623034792@qq.com>
2022-01-15 13:37:42 +0800

    add the interface and param description

--- service/service_edu/src/main/java/com/scut/lzx/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/scut/lzx/eduservice/controller/EduTeacherController.java
@@ -3,6 +3,8 @@ package com.scut.lzx.eduservice.controller;
 
 import com.scut.lzx.eduservice.entity.EduTeacher;
 import com.scut.lzx.eduservice.service.EduTeacherService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,13 +24,15 @@ public class EduTeacherController {
     @Autowired
     private EduTeacherService eduTeacherService;
 
+    @ApiOperation(value = "获取所有讲师信息")
     @GetMapping("findAll")
-    public List<EduTeacher> findAllTeacher(){
+    public List<EduTeacher> findAllTeacher() {
         return eduTeacherService.list(null);
     }
 
+    @ApiOperation(value = "根据id删除讲师信息")
     @DeleteMapping("deleteById/{id}")
-    public boolean deleteTeacher(@PathVariable String id){
+    public boolean deleteTeacher(@ApiParam(name = "id", value = "讲师ID", required = true) @PathVariable String id) {
         return eduTeacherService.removeById(id);
     }
 }
