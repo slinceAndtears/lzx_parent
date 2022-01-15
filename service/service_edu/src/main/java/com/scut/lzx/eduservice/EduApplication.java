2e3a29aaafc48936a032cd095b2917646efafb17
rookie <623034792@qq.com>
2022-01-15 13:30:07 +0800

    add the swagger

--- service/service_edu/src/main/java/com/scut/lzx/eduservice/EduApplication.java
+++ service/service_edu/src/main/java/com/scut/lzx/eduservice/EduApplication.java
@@ -2,8 +2,12 @@ package com.scut.lzx.eduservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackages = {"com.scut"})
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class, args);
