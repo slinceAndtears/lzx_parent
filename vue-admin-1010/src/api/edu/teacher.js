f9a23518ffe0cf581b6bf0d91d10ac94e2ec0c1e
rookie <623034792@qq.com>
2022-01-18 17:04:51 +0800

    add the teadcher page

--- vue-admin-1010/src/api/edu/teacher.js
+++ vue-admin-1010/src/api/edu/teacher.js
@@ -2,20 +2,20 @@ import request from '@/utils/request'
 export default {
     //1 讲师列表（条件查询分页）
     //current当前页 limit每页记录数 teacherQuery条件对象
-    getTeacherListPage(current,limit,teacherQuery) {
+    getTeacherListPage(current, limit, teacherQuery) {
         return request({
             //url: '/eduservice/teacher/pageTeacherCondition/'+current+"/"+limit,
             url: `/eduservice/teacher/pageTeacherCondition/${current}/${limit}`,
             method: 'post',
             //teacherQuery条件对象，后端使用RequestBody获取数据
-            //data表示把对象转换json进行传递到接口里面
+            //data表示把对象转换json进行传递到后端接口里面
             data: teacherQuery
           })
     },
     //删除讲师
     deleteTeacherId(id) {
         return request({
-            url: `/eduservice/teacher/${id}`,
+            url: `/eduservice/teacher/deleteById/${id}`,
             method: 'delete'
           })
     },
